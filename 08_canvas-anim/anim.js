38cc52c7dca8ca49318e3489e65d2727fafecd59
jwu03 <jwu03@stuy.edu>
2020-02-13 22:21:50 -0500

    finish

--- 08_canvas-anim/anim.js
+++ 08_canvas-anim/anim.js
@@ -1,9 +1,8 @@
 // Jionghao Wu
-// SoftDev Pd. 2
-// K #07: They lock us in the tower whenever we get caught
-// 2020-02-12
+// SoftDev pd2
+// K08: What is it saving the screen from
+// 2020-02-14
 
-//retrieve node in DOM via ID
 
 
 //instantiate a Canvas Rendering 2D object
@@ -13,6 +12,7 @@ var radius = 0;
 var stop = true;
 var id = null;
 var increase = true;
+var state = null;
 //invoke interface methods
 ctx.fillStyle="#ff0000" //red
 //ctx.fillRect(50,50,100,200)
@@ -34,26 +34,93 @@ var animate = function(event){
   else{
     radius--;
   }
-  id = requestAnimationFrame(animate);
+  id1 = requestAnimationFrame(animate);
   stop = false;
+  state = 1;
 };
+
+
+
+var xcor = 100;
+var ycor = 100;
+var dirList = ['SE','SW','NE','NW']
+var direction = 'NE';
+var dvdjpg = document.getElementById("dvd-image");
+
+var randomDvd = function() {
+    xcor = Math.floor(Math.random() * 400);
+    ycor = Math.floor(Math.random() * 400);
+    direction = dirList[Math.floor(Math.random() * 4)];
+}
+
+var go = function(){
+  randomDvd();
+  DVDanim();
+}
+
+var DVDanim = function(event){
+  id2 = window.requestAnimationFrame(DVDanim);
+  ctx.clearRect(0,0,c.width,c.height);
+  ctx.drawImage(dvdjpg,xcor,ycor,100,70);
+  if(xcor == 500) {
+    if(direction == "NE"){
+      direction = "NW";}
+    if(direction == "SE"){
+      direction = "SW";}
+  }
+  if(xcor == 0) {
+    if(direction == "NW"){
+      direction = "NE";}
+    if(direction == "SW"){
+      direction = "SE";}
+  }
+  if(ycor == 530) {
+    if(direction == "SE"){
+      direction = "NE";}
+    if(direction == "SW"){
+      direction = "NW";}
+  }
+  if(ycor == 0) {
+    if(direction == "NE"){
+      direction = "SE";}
+    if(direction == "NW"){
+      direction = "SW";}
+  }
+
+  if(direction == "NE"){
+    xcor += 1;
+    ycor -= 1;
+  }
+  if(direction == "SE"){
+    xcor += 1;
+    ycor += 1;
+  }
+  if(direction == "NW"){
+    xcor -= 1;
+    ycor -= 1;
+  }
+  if(direction == "SW"){
+    xcor -= 1;
+    ycor += 1;
+  }
+  stop = false;
+  state = 2;
+}
 
 var halt = function(event){
   if (stop){
     event.preventDefault();
   }
   else{
-    cancelAnimationFrame(id);
+    if (state == 1){
+    window.cancelAnimationFrame(id1);
+    }
+    if(state == 2){
+    window.cancelAnimationFrame(id2);
+    }
     stop = true;
   }
 }
-var Cwidth = c.width
-var Cheight = c.height;
-
-var DVDanim = function(event){
-  ctx.clearRect(0,0,c.width,c.height);
-  ctx.fillRect((Math.random() * Cwidth),(Math.random() * Cheight),100,50)
-}
 
 
 
@@ -62,7 +129,6 @@ var DVDanim = function(event){
 
 
 
-  
 var button1 = document.getElementById('animate');
 button1.addEventListener('click', function(event){
   if (!stop){
@@ -77,4 +143,11 @@ var button2 = document.getElementById('stop');
 button2.addEventListener('click', halt);
 
 var button3 = document.getElementById('DVD');
-button3.addEventListener('click', DVDanim);
+button3.addEventListener('click', function(event){
+  if (!stop){
+    event.preventDefault();
+  }
+  else{
+    go();
+  }
+});
